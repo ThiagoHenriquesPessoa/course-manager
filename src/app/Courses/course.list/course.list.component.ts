962fa88550cc3f110c589a041aa66973b3fe84f5
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2024-04-04 23:19:47 -0300

    criando componente

---
+++ src/app/Courses/course.list/course.list.component.ts
@@ -0,0 +1,14 @@
+import { Component  } from '@angular/core';
+
+
+@Component({
+  selector: 'app-course.list',
+  standalone: true,
+  imports: [],
+  templateUrl: './course.list.component.html',
+  styleUrl: './course.list.component.css',
+  template: '<h2> Couse List </h2>'
+})
+export class CourseListComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { CourseListComponent } from './Courses/course.list/course.list.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, FormsModule, CourseListComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class AppComponent {
   title = 'course-manager';
+
+  name: string = 'Thiago Henriques';
 }
